0ab9d4adcfafc132c85ac26c7548e32b134715d2
jorgeruiz27 <jorge.cx.27@gamil.com>
2022-05-24 20:54:52 -0400

    finalizando

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from core.models import Pessoa
 # Register your models here.
+
+class PessoaAdmin (admin.ModelAdmin):
+    list_display = ('nome', 'idade')
+    list_filter = ('nome',)
+
+admin.site.register(Pessoa)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=200)
+    idade = models.IntegerField()
+
+# pessoa = Pessoa.objects.get(nome=='Rafael')
